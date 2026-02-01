7fcf84f755e7c3f57223966ce8b6520680b35410
Saimir Alla <allasaimir44@gmail.com>
2026-02-01 20:23:09 +0200

    feat: create user service

---
+++ src/main/java/com/saimiral/usermanagement/service/UserService.java
@@ -0,0 +1,20 @@
+package com.saimiral.usermanagement.service;
+
+import com.saimiral.usermanagement.entity.User;
+import com.saimiral.usermanagement.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> getAllUsers(){
+        return repository.findAll();
+    }
+}
