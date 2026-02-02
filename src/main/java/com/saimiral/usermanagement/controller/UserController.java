8d4b955d18498a84de46d41679895e131b61dc9d
Saimir Alla <allasaimir44@gmail.com>
2026-02-02 22:51:52 +0200

    feat: add update user endpoint

--- src/main/java/com/saimiral/usermanagement/controller/UserController.java
+++ src/main/java/com/saimiral/usermanagement/controller/UserController.java
@@ -3,6 +3,7 @@ package com.saimiral.usermanagement.controller;
 
 import com.saimiral.usermanagement.dto.UserCreateDTO;
 import com.saimiral.usermanagement.dto.UserResponseDTO;
+import com.saimiral.usermanagement.dto.UserUpdateDTO;
 import com.saimiral.usermanagement.service.UserServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -29,8 +30,19 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<void> deleteUser(@PathVariable Long id){
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
         service.deleteUSer(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto){
+        UserResponseDTO updateUser = service.updateUser(id, dto);
+        return ResponseEntity.ok(updateUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getUserById(id));
     }
 }
---
+++ src/main/java/com/saimiral/usermanagement/dto/UserUpdateDTO.java
@@ -0,0 +1,28 @@
+package com.saimiral.usermanagement.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public class UserUpdateDTO {
+    @NotBlank
+    private String name;
+
+    @Min(18)
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
